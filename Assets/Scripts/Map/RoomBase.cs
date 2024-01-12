d3d4bfac189e881df249b3e5356ceda2674ac8e5
Jung BoBae <acarid27@gmail.com>
2024-01-12 19:29:54 +0900

    feat: Add Room Prefabs

---
+++ Assets/Scripts/Map/RoomBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBase : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public Transform[] paths;
+}
